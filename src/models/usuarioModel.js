fd13c252254bde7f1654e5547495ef629d472fbe
helombrito <heloisa.brito@sptech.school>
2022-05-05 22:11:36 -0300

    heloisa - correção na conexão do cadastro com api

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -21,13 +21,18 @@ function entrar(email, senha) {
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
 function cadastrar(email, nome, cnpj, tel, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", email, nome, cnpj, tel, senha);
-    
+
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO shopping (email, nome, cnpj, tel, senha) VALUES ('${email}', '${nome}', '${cnpj}', '${tel}', '${senha}');
+        INSERT INTO shopping (nome, cnpj, tel) VALUES ('${nome}', '${cnpj}', '${tel}');
+        
     `;
+    var instrucao2 = `INSERT INTO usuario (emailusuario, senhausuario) VALUES ('${email}', '${senha}'); 
+    `;
+
     console.log("Executando a instrução SQL: \n" + instrucao);
+    database.executar(instrucao2);
     return database.executar(instrucao);
 }
 
